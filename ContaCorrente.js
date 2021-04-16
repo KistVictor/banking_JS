85324971a0232ab10f37db5e8b869e4ffe6a172d
Victor Kist <victoraugustokist@gmail.com>
2021-04-16 18:36:49 -0300

    add get e set

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,21 +1,40 @@
+import { Cliente } from "./Cliente.js"
+
 export class ContaCorrente{
     agencia
-    #saldo = 0 //Não fica exposto fora da classe.
+    _cliente
+
+    set cliente(novoValor){                     //Proteção do _cliente. Só pode receber instância do Cliente
+        if(novoValor instanceof Cliente)
+            this._cliente = novoValor
+    }
+
+    get cliente(){
+        return this._cliente
+    }
+
+    _saldo = 0 //Não fica exposto fora da classe.
+
+    get saldo(){
+        return this._saldo
+    }
 
     sacar(valor){
-        if(this.#saldo >= valor)
-            this.#saldo -= valor
+        if(this._saldo >= valor)
+            this._saldo -= valor
             return valor
     }
 
     depositar(valor){
 
         if(valor <= 0) return
-
-        this.#saldo += valor;
+        this._saldo = this._saldo + valor;
     }
 
-
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor)
+        conta.depositar(valorSacado)
+    }
 
 
 
--- index.js
+++ index.js
@@ -2,16 +2,27 @@ import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente()
-const cliente2 = new Cliente()
-const ContaCorrenteVictor = new ContaCorrente()
+const conta1 = new ContaCorrente()
 
 cliente1.nome = "Victor"
 cliente1.cpf = 11122233309
+conta1.cliente = cliente1
+conta1.agencia = 1001
+
+//conta1.#saldo = 100 //Não funciona pois so pode ser mexido dentro da fução
+
+const cliente2 = new Cliente()
+const conta2 = new ContaCorrente()
 
 cliente2.nome = "Alice"
 cliente2.cpf = 88822233309
+conta2.cliente = cliente2
+conta2.agencia = 1001
 
-ContaCorrenteVictor.agencia = 1001
-//ContaCorrenteVictor.#saldo = 100 //Não funciona pois so pode ser mexido dentro da fução
-ContaCorrenteVictor.depositar(-100)
-console.log(ContaCorrenteVictor)
+
+
+conta1.depositar(500)
+conta1.transferir(200, conta2)
+
+console.log(conta1)
+console.log(conta1.cliente)
