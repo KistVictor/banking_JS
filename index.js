e357dae66cfbb1f6a038f898635f6f89a2ed91c3
Victor Kist <victoraugustokist@gmail.com>
2021-04-18 09:52:03 -0300

    add funcionarios e organizacao em pastas

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
-import { ContaPoupanca } from "./ContaPoupanca.js"
+import {ContaCorrente} from "./Conta/ContaCorrente.js"
+import {ContaPoupanca} from "./Conta/ContaPoupanca.js"
+import {Gerente} from "./Funcionarios/Gerente.js"
+import {Diretor} from "./Funcionarios/Diretor.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente("Victor", "03154864210")
-const contaCorrente1 = new ContaCorrente(cliente1, 1001)
-const contaPoupanca1 = new ContaPoupanca(50, cliente1, 1001)
+const diretor = new Diretor("Roberto", 10000, 12356486321)
+diretor.cadastrarSenha("123")
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123")
 
-console.log(contaCorrente1)
-console.log(contaPoupanca1)
+const cliente = new Cliente("Victor", 12356486321, "123")
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "123")
+
+console.log(clienteEstaLogado)
+console.log(cliente)
+console.log(diretor)
